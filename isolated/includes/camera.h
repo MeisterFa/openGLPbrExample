c1dfa6603d0b8511c8850069f2cbc0f96dc2ceab
Fabian Meister <meisterfa68426@th-nuernberg.de>
2020-11-13 19:34:01 +0100

    fixed camera from defries (-90 is not cool)

--- isolated/includes/camera.h
+++ isolated/includes/camera.h
@@ -20,7 +20,7 @@ enum Camera_Movement {
 };
 
 // Default camera values
-const float YAW         =  -90.0f;
+const float YAW         =  0.0f;
 const float PITCH       =  0.0f;
 const float SPEED       =  2.5f;
 const float SENSITIVITY =  0.1f;
@@ -126,9 +126,9 @@ private:
     {
         // calculate the new Front vector
         glm::vec3 front;
-        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        front.x = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
         front.y = sin(glm::radians(Pitch));
-        front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        front.z = ((-1) * cos(glm::radians(Yaw))) * cos(glm::radians(Pitch));
         Front = glm::normalize(front);
         // also re-calculate the Right and Up vector
         Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
